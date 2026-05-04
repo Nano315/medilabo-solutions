e61df58d7ac4eb183f6af15ddfa44603f5b31433
Nano315 <valentin3135@gmail.com>
2026-05-04 23:45:10 +0200

    Dockerisation du projet : Dockerfiles multi-stage pour les microservices, nginx pour le front, docker-compose orchestrant l'ensemble et externalisation des URLs en variables d'environnement

--- assessment/src/main/java/com/medilabo/assessment/client/NoteClient.java
+++ assessment/src/main/java/com/medilabo/assessment/client/NoteClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "note", url = "http://localhost:8082", configuration = FeignConfig.class)
+@FeignClient(name = "note", url = "${note.service.url}", configuration = FeignConfig.class)
 public interface NoteClient {
 
     @GetMapping("/api/notes/patient/{patId}")
--- assessment/src/main/java/com/medilabo/assessment/client/PatientClient.java
+++ assessment/src/main/java/com/medilabo/assessment/client/PatientClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "patient", url = "http://localhost:8080", configuration = FeignConfig.class)
+@FeignClient(name = "patient", url = "${patient.service.url}", configuration = FeignConfig.class)
 public interface PatientClient {
 
     @GetMapping("/api/patients/{id}")
